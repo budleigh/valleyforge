e4b1a84e951fc6ced0cca0aeb8524e356902ae84
budleigh <arguewithplato@gmail.com>
2016-07-11 12:37:46 -0700

    experimenting with vue/layout

--- static/js/valleyforge.js
+++ static/js/valleyforge.js
@@ -1,3 +1,27 @@
 /**
  * Created by budleigh on 7/11/16.
  */
+
+Vue.config.delimiters = ['[[', ']]']; // clashes with template {}
+
+Vue.component('search', {
+    template: '#search',
+    data: function () {
+        return {
+            words: ''
+        }
+    }
+});
+
+Vue.component('anagrams', {
+    template: '#anagrams',
+    data: function () {
+        return {
+            anagrams: []
+        }
+    }
+});
+
+var app = new Vue({
+    el: '#app'
+});
