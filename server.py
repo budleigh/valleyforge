c701ffc0307b48cdf83a79619490a9f6c28bd710
budleigh <arguewithplato@gmail.com>
2016-07-11 11:24:17 -0700

    hello world server

---
+++ server.py
@@ -0,0 +1,19 @@
+import os
+import tornado.ioloop
+import tornado.web
+
+
+class IndexHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.write('yo')
+
+
+def make_app():
+    return tornado.web.Application([
+        (r'/', IndexHandler),
+    ])
+
+if __name__ == '__main__':
+    app = make_app()
+    app.listen(os.environ.get('PORT', 5000))
