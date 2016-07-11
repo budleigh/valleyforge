0d631ca4dd5be8ba8ec17729906c54bb94bc631d
budleigh <arguewithplato@gmail.com>
2016-07-11 11:59:19 -0700

    serve index/static files

--- server.py
+++ server.py
@@ -6,12 +6,15 @@ import tornado.web
 class IndexHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.write('yo')
+        self.render('templates/index.html')
 
 
 def make_app():
     return tornado.web.Application([
         (r'/', IndexHandler),
+        (r'/static/(.*)', tornado.web.StaticFileHandler, {
+            'path': os.path.dirname(os.path.realpath(__file__)) + '/static',
+        }),
     ])
 
 if __name__ == '__main__':
