bb8b1d143da6b083ac87cdad63f7db5e10fe94d8
budleigh <arguewithplato@gmail.com>
2016-07-11 18:56:10 -0700

    first draft of space-adding engine

--- src/permute.py
+++ src/permute.py
@@ -1,4 +1,5 @@
 import threading
+import queue
 import itertools
 
 
@@ -26,15 +27,68 @@ class PermutationThread(threading.Thread):
 
     def run(self):
         self.normalize_words()
-        for anagram in itertools.permutations(self.words):
+        workers = []
+        q = queue.Queue()
+        for x in range(len(self.words)):
+            modified = self.words
+            for i in range(x):
+                modified += ' '
+            sw = SpaceWorker(q, modified, self)
+            sw.start()
+            workers.append(sw)
+
+        while True:
             if self.kill:
                 break
 
-            anagram = ''.join(list(anagram))
-            if anagram in self.dictionary:
-                if anagram in self.seen:
-                    continue
+            anagram = None
+            try:
+                anagram = q.get_nowait()
+            except queue.Empty:
+                pass
+            if anagram:
                 self.message(anagram)
-                self.seen.add(anagram)
+
+            for worker in workers:
+                if not worker.running:
+                    workers.remove(worker)
+            if not workers:
+                break
 
         self.socket.close()
+
+
+class SpaceWorker(threading.Thread):
+
+    def __init__(self, queue, words, parent):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.words = words
+        self.parent = parent
+        self.seen = set()
+        self.running = False
+
+    def is_english(self, word):
+        return word in self.parent.dictionary
+
+    def run(self):
+        self.running = True
+        for anagram in itertools.permutations(self.words):
+            if self.parent.kill:
+                break
+
+            anagram = ''.join(list(anagram))
+            valid = True
+            for word in anagram.split(' '):
+                if not self.is_english(word):
+                    valid = False
+                    break
+            if valid:
+                if anagram in self.seen:
+                    continue
+                else:
+                    self.seen.add(anagram)
+                    self.queue.put(anagram)
+
+        self.running = False
+
